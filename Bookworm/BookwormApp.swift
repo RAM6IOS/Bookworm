8539040df65a72a20099a58c2ce310724e562e9d
RAM6IOS <ios191169@gmail.comm>
2022-05-05 11:11:20 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Bouchedoub Rmazi on 5/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Bouchedoub Rmazi on 5/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
